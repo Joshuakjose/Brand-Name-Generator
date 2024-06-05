a9bce7e99993cad8bc7e925ae71f755d444e1c60
Joshuakjose <Joshuakjose17@gmail.com>
2024-06-06 01:10:22 +0530

    Add files via upload

---
+++ Brand Name Generator.py
@@ -0,0 +1,11 @@
+print("Click Run to run the final project you will build")
+print("WELCOME to the brand name generator")
+#1. Ask the user for the city that they grew up in.
+city=input("enter the city you grew up in\n")
+
+#2. Ask the user for the name of a pet.
+pet=input("enter the name of your pet\n")
+
+
+#3. Combine the name of their city and pet and show them their band name.
+print("the brand name would be " + city +" "+ pet)
